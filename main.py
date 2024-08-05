1c68c21e807b100cf06e105f43647de1fc6c8a6c
saidjon <saidjonbahromov@gmail.com>
2024-08-05 18:02:44 +0500

    Сохтани тугмаҳо дар телеграм

--- main.py
+++ main.py
@@ -1,22 +1,60 @@
-from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
 TOKEN = '7249517311:AAFKEqGOkugTiEmeXQ6Q3LrC1ValfilLgv8'
 
 def start(update, context):
-    message = ('Ман Баҳромов Саидҷон. Хуш омадед ба боти ман! \n\n'
-               '/contact - Отправить вам мой контакты.\n'
-               '/talk - Суҳбат дар Whatsapp')
+    message = ('Ман Баҳромов Саидҷон. Хуш омадед ба боти ман!')
 
+    keyboard = [
+        [InlineKeyboardButton('Контакты', callback_data='contact')],
+        [InlineKeyboardButton('Суҳбат', callback_data='talk')],
+        [InlineKeyboardButton('Messenger', callback_data='messed')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
     with open('photo.jpg', 'rb') as file:
-        update.message.reply_photo(file, message)
+        update.message.reply_photo(file, message, reply_markup = reply_markup)
+
+def main_page(update, context):
+    message = ('Ман Баҳромов Саидҷон. Хуш омадед ба боти ман!')
+    keyboard = [
+        [InlineKeyboardButton('Контакты', callback_data='contact')],
+        [InlineKeyboardButton('Суҳбат', callback_data='talk')],
+        [InlineKeyboardButton('Messenger', callback_data='messed')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    quety = update.callback_query
+    quety.edit_message_caption(message, reply_markup)
 
 def contact(update, context):
-    message = '+992 988851282 \n +992 100300658'
-    update.message.reply_text(message)
+    message = '+992 988851282 \n+992 100300658'
+    quety = update.callback_query
+    keyboard = [
+        [InlineKeyboardButton('Назад', callback_data='main')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    quety.edit_message_caption(message, reply_markup = reply_markup)
 
 def talk(update, context):
-    message = 'Ман ҳоло ҷавоб дода наметавонам. :joy:'
-    update.message.reply_text(message)
+    message = 'Ман ҳоло ҷавоб дода наметавонам!'
+    quety = update.callback_query
+    keyboard = [
+        [InlineKeyboardButton('Назад', callback_data='main')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    quety.edit_message_caption(message, reply_markup = reply_markup)
+
+def messed(update, context):
+    
+    message = 'Ман дар месенджерҳо'
+    quety = update.callback_query
+    keyboard = [
+        [InlineKeyboardButton('Telegram', url='https://t.me/saidjonbahromov'),
+         InlineKeyboardButton('Whathapp', url='https://whatsapp.com/dl/code=gb1JTMJy2Y')],
+         [InlineKeyboardButton('Назад', callback_data='main')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    quety.edit_message_caption(message, reply_markup = reply_markup)
 
 def main():
     updater = Updater(TOKEN)
@@ -25,7 +63,10 @@ def main():
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('contact', contact))
     dispatcher.add_handler(CommandHandler('talk', talk))
-
+    dispatcher.add_handler(CallbackQueryHandler(contact, pattern='contact'))
+    dispatcher.add_handler(CallbackQueryHandler(main_page, pattern='main'))
+    dispatcher.add_handler(CallbackQueryHandler(talk, pattern='talk'))
+    dispatcher.add_handler(CallbackQueryHandler(messed, pattern='messed'))  
     updater.start_polling()
     updater.idle()
 
